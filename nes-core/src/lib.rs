c6aa11de9784a3629a946513596524abc1fe39da
takuchalle <me@takuchalle.dev>
2025-04-17 14:07:53 +0900

    Add Nes struct

--- nes-core/src/lib.rs
+++ nes-core/src/lib.rs
@@ -1,2 +1,14 @@
 pub mod cpu;
 pub mod opcodes;
+
+pub struct Nes {
+    cpu: cpu::CPU,
+}
+
+impl Nes {
+    pub fn new(rom: &[u8]) -> Nes {
+        let mut cpu = cpu::CPU::new();
+        cpu.load(rom);
+        Self { cpu }
+    }
+}
